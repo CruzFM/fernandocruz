539743da0686aa7447d0c27863b363e47f112b16
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-11-14 00:43:40 -0300

    ContactMe component finished.

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,7 +1,42 @@
-function ContactMe (){ 
-    return(
-        <div>Soy Contact me, contactame gato.</div>
-    );
+import gmailSvg from "../assets/gmail-svg.svg";
+import linkedinSvg from "../assets/linkedin-svg.svg";
+import downloadSvg from "../assets/download-svg.svg";
+import resumeSvg from "../assets/cv-pdf.svg";
+
+function ContactMe() {
+  return (
+    <div className="hero bg-base-200 lg:py-24 xl:py-0  xl:min-h-screen ">
+      <div className="hero-content md:text-center">
+        <div className="max-w-md">
+          <h2 className="text-2xl md:text-4xl font-semibold">
+            04. Get in touch
+          </h2>
+          <p className="pt-6 md:text-lg">
+            I love connecting with people, so whether you're looking for a
+            developer for your next project or simply want to chat about code,
+            feel free to reach out.
+          </p>
+          <p className="py-3 md:text-lg">
+            Let's grab a virtual coffee and discuss how we can work together!
+          </p>
+          <div className="flex flex-row items-center justify-center gap-3 mt-6">
+            <a href="mailto:cruzfernandomartin94@gmail.com" className="btn rounded-none btn-square btn-outline hover:bg-neutral-300">
+              <img src={gmailSvg} alt="Gmail Icon" className="h-6 w-6" />
+            </a>
+            <a href="https://www.linkedin.com/in/cruzfernandomartin/" rel="noopener noreferrer" target="_blank" className="btn rounded-none btn-square btn-outline hover:bg-neutral-300">
+              <img src={linkedinSvg} alt="LinkedIn Icon" className="h-6 w-6" />
+            </a>
+            <a href="/Fernando-Cruz-resume.pdf" target="_blank" rel="noopener noreferrer" className="btn rounded-none btn-square btn-outline hover:bg-neutral-300">
+              <img src={resumeSvg} alt="View Resume Icon" className="h-7 w-7" />
+            </a>
+            <a href="/Fernando-Cruz-resume.pdf" download className="btn rounded-none btn-square btn-outline hover:bg-neutral-300">
+              <img src={downloadSvg} alt="Download Resume Icon" className="h-7 w-7" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
             <h1 className="text-4xl md:text-7xl font-extrabold text-primary-color">Fernando Martin Cruz</h1>
             <h2 className="text-xl md:text-4xl font-semibold">Software developer</h2>
             <p className="text-normal md:text-2xl font-normal mt-2.5">Coding with passion, delivering with precision.</p>
-            <button type="button" className="btn-primary w-48 py-2 md:py-4 md:px-5 rounded-md mt-2.5 font-semibold flex justify-center items-center">Selected projects</button>
+            <button type="button" className="btn-primary w-48 py-2 md:py-4 md:px-5 btn rounded-none mt-2.5 font-semibold flex justify-center items-center">Selected projects</button>
         </div>
     );
 }
